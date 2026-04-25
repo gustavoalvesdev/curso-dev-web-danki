b8f54c1e5172e727a5b55a7ef4859c44e18f90d4
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2026-04-25 18:51:56 -0300

    HTML5 e Marcação 2/2

--- Projeto_01/index.php
+++ Projeto_01/index.php
@@ -9,52 +9,147 @@
 </head>
 <body>
     <header>
-        <div class="logo">Logomarca</div>
-        <!-- logo -->
-         <nav class="desktop">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-         </nav>
-         <!-- desktop -->
-         <nav class="mobile">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-         </nav>
-         <!-- mobile -->
+        <div class="center">
+            <div class="logo">Logomarca</div>
+            <!-- logo -->
+             <nav class="desktop">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+             </nav>
+             <!-- desktop -->
+             <nav class="mobile">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+             </nav>
+             <!-- mobile -->
+        </div>
+        <!-- center -->
     </header>
 
     <section class="banner-principal">
-        <form>
-            <h2>Qual o seu melhor e-mail?</h2>
-            <input type="email" name="email" required>
-            <input type="submit" name="acao" value="Cadastrar">
-        </form>
+        <div class="center">
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required>
+                <input type="submit" name="acao" value="Cadastrar">
+            </form>
+        </div>
+        <!-- center -->
     </section>
     <!-- banner-principal -->
      
     <section class="descricao-autor">
-        <div class="w50">
-            <h2>Guilherme C. Grillo</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi id officiis quod consectetur vitae dignissimos illum at, nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque? nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi id officiis quod consectetur vitae dignissimos illum at, nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque? nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque?
-            </p>
+        <div class="center">
+            <div class="w50">
+                <h2>Guilherme C. Grillo</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi id officiis quod consectetur vitae dignissimos illum at, nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque? nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi id officiis quod consectetur vitae dignissimos illum at, nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque? nemo laborum eligendi architecto voluptas numquam odio voluptate asperiores, ullam impedit quae. Neque?
+                </p>
+            </div>
+            <!-- w50 -->
+            <div class="w50">
+                <img src="images/foto.jpg">
+            </div>
+            <!-- w50 -->
         </div>
-        <!-- w50 -->
-        <div class="w50">
-            <img src="images/foto.jpg">
-        <!-- w50 -->
+        <!-- center -->
     </section>
     <!-- descricao-autor -->
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <!-- title -->
+            <div class="box-especialidade">
+                <h3>icone1</h3>
+                <h3>CSS3</h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis est, ipsum nulla sed, optio amet labore libero explicabo ex, doloribus obcaecati quia repellat enim! Tempore delectus explicabo repellendus error similique!
+                </p>
+            </div>
+            <!-- box-especialidade -->
+            <div class="box-especialidade">
+                <h3>icone2</h3>
+                <h3>HTML5</h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis est, ipsum nulla sed, optio amet labore libero explicabo ex, doloribus obcaecati quia repellat enim! Tempore delectus explicabo repellendus error similique!
+                </p>
+            </div>
+            <!-- box-especialidade -->
+            <div class="box-especialidade">
+                <h3>icone3</h3>
+                <h3>JavaScript</h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis est, ipsum nulla sed, optio amet labore libero explicabo ex, doloribus obcaecati quia repellat enim! Tempore delectus explicabo repellendus error similique!
+                </p>
+            </div>
+            <!-- box-especialidade -->
+        </div>
+        <!-- center -->
+    </section>
+    <!-- especialidades -->
+    <section class="extras">
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+                <!-- title -->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, voluptatum! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, aut non doloribus officiis veritatis in quaerat quos magnam necessitatibus odit ipsam, odio dolores nihil sapiente, exercitationem vitae nemo quia ipsa!</p>
+                    <!-- depoimento-descricao -->
+                    <p class="nome-autor">Lorem Ipsum</p>
+                    <!-- nome-autor -->
+                </div>
+                <!-- depoimento-single -->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, voluptatum! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, aut non doloribus officiis veritatis in quaerat quos magnam necessitatibus odit ipsam, odio dolores nihil sapiente, exercitationem vitae nemo quia ipsa!</p>
+                    <!-- depoimento-descricao -->
+                    <p class="nome-autor">Lorem Ipsum</p>
+                    <!-- nome-autor -->
+                </div>
+                <!-- depoimento-single -->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, voluptatum! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, aut non doloribus officiis veritatis in quaerat quos magnam necessitatibus odit ipsam, odio dolores nihil sapiente, exercitationem vitae nemo quia ipsa!</p>
+                    <!-- depoimento-descricao -->
+                    <p class="nome-autor">Lorem Ipsum</p>
+                    <!-- nome-autor -->
+                </div>
+                <!-- depoimento-single -->
+            </div>
+            <!-- w50 -->
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+                <!-- title -->
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+                    </ul>
+                </div>
+                <!-- servicos -->
+            </div>
+            <!-- w50 -->
+        </div>
+        <!-- center -->
+    </section>
+    <!-- extras -->
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div>
+        <!-- center -->
+    </footer>
 </body>
 </html>
